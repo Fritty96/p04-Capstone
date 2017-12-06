c63237fbc6e15fd5c43313b2925012d44197bb69
Fritty96 <31226164+Fritty96@users.noreply.github.com>
2017-12-06 12:13:41 -0800

    Create prompt.h

---
+++ prompt.h
@@ -0,0 +1,20 @@
+#ifindef _PROMPT_H_
+#define _PROMPT_H_
+
+#include <string>
+#include <iostream>
+
+class prompt {
+public:
+setText(string);
+getIntResponse();
+getStringResponse();
+showResponse();
+
+private:
+int number();
+std:: string word();
+
+};
+
+#endif
