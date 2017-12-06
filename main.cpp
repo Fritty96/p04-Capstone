526437d5a6431084873bbb3443b6d670d5bb4df1
Fritty96 <31226164+Fritty96@users.noreply.github.com>
2017-12-06 12:10:20 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+#include<vector>
+
+
+using namespace std;
+
+
+int main (){
+//Retrieve and Store User Words
+vector <prompt> all;
+prompt first;
+first.setText("Name");
+first.getStringResponse();
+all.push_back(first);
+
+//repeat
+
+
+return 0;
+}
