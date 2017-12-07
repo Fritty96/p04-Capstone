266d592d4b1817ae09c313d0acf4ee0e3861e9ed
Fritty96 <mphillips39@mail.csuchico.edu>
2017-12-07 14:19:50 -0800

    more

--- main.cpp
+++ main.cpp
@@ -18,17 +18,93 @@ all.push_back(first);
 
 //repeat
 prompt second;
+second.setText("Noun");
+second.getStringResponse();
+all.push_back(second);
+
+prompt third;
+third.setText("LNumber");
+third.getIntResponse();
+all.push_back(third);
+
+prompt fourth;
+fourth.setText("Title");
+fourth.getStringResponse();
+all.push_back(fourth);
+
+prompt fifth;
+fifth.setText("Adjective");
+fifth.setStringResponse();
+all.push_back(fifth);
+
+prompt sixth;
+sixth.setText("Adj2");
+sixth.setStringResponse();
+all.push_back(sixth);
+
+
+prompt seven;
+seven.setText("Animal");
+seven.setStringResponse();
+all.push_back(seven);
+
+
+prompt eight;
+eight.setText("Animal2");
+eight.setStringResponse();
+all.push_back(eight);
+
+
+prompt nine;
+nine.setText("LPlace");
+nine.setStringResponse();
+all.push_back(nine);
+
+
+prompt ten;
+tem.setText("Number");
+ten.setIntResponse();
+all.push_back(ten);
+
+
+prompt eleven;
+eleven.setText("Num2");
+eleven.setIntResponse();
+all.push_back(eleven);
+
+
+prompt twelve;
+twelve.setText("MOT");
+twelve.setStringResponse();
+all.push_back(twelve);
+
+
+prompt thirteen;
+thirteen.setText("Adj3");
+thirteen.setStringResponse();
+all.push_back(thirteen);
+
+
+
 
 
 //Start Story
 
-Story();
+Story(vector<prompt> all);
+
 
 return 0;
 }
 
 
-void Story(){
+void Story(vector<prompt> all){
 
 
 }
+
+coutfile(){
+//cout and save to file at same time.
+
+}
+
+
