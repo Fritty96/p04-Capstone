42659003531cd0fa6f9c790606c3b3b400d26975
Fritty96 <mphillips39@mail.csuchico.edu>
2017-12-08 12:10:11 -0800

    moore

--- prompt.cpp
+++ prompt.cpp
@@ -7,53 +7,53 @@
 
 
 void prompt::setText(std::string text){
-if text = "Name"{
+if (text = "Name"){
 `cout << "Welcome, Please enter your name" << endl;
     }   
-else if text = "Noun"{
+else if (text = "Noun"){
     cout << "Please enter a noun" << endl;
     }
-else if text = "LNumber"{
+else if (text = "LNumber"){
     cout << "Please enter a Large Number" << endl;
     }
 
-else if text = "Title"{
+else if (text = "Title"){
     cout << "Please enter a Title (Sir, King, etc.)" << endl;
     }
 
-else if text = "Adjective"{
+else if (text = "Adjective"){
 
     cout << "Please enter an adjective" << endl;
     }
-else if text = "Adj2"{
+else if (text = "Adj2"){
 
     cout << "Please enter an adjective" << endl;
     }
-else if text = "Animal"{
+else if (text = "Animal"){
 
     cout << "Please enter an animal" << endl;
     }
-else if text = "Animal2"{
+else if (text = "Animal2"){
 
     cout << "Please enter an animal" << endl;
     }
-else if text = "LPlace"{
+else if (text = "LPlace"){
 
     cout << "Please enter a large place" << endl;
     }
-else if text = "Number"{
+else if (text = "Number"){
 
     cout << "Please enter a number" << endl;
     }
-else if text = "Num2"{
+else if (text = "Num2"){
 
     cout << "Please enter a number" << endl;
     }
-else if text = "MOT"{
+else if (text = "MOT"){
 
     cout << "Please enter a measurement of time (Seconds, Hours, etc)" << endl;
     }
-else if text = "Adj3"{
+else if (text = "Adj3"){
 
     cout << "Please enter an adjective" << endl;
     }
@@ -62,19 +62,21 @@ else if text = "Adj3"{
 }
 
 
-void prompt::getStringResponse(){
+std::string prompt::getStringResponse(){
 cin >> word;
 
 }
 
 
-void prompt::getIntResponse(){
+int prompt::getIntResponse(){
 cin >> number;
 
 }
 
-prompt::showResponse(){
+int prompt::showIntResponse( ){
+return number;
+}
 
-
-
+std::string prompt::showStringResponse(){
+return word;
 }
